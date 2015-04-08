f2005f1b0f46ac57c434119d44fe87b776cb0020
hsirrk341 <hsirrk341@gmail.com>
2015-04-08 18:31:26 +0000

    pending validations

--- app/models/pending.rb
+++ app/models/pending.rb
@@ -20,4 +20,18 @@
 #
 
 class Pending < ActiveRecord::Base
+	 
+	validates :contract_id, presence: true,   length: {maximum: 100 }
+	validates :uid, presence: true,   length: {maximum: 100 }
+	validates :firstName, presence: true,   length: {maximum: 100 }
+	validates :lastName, presence: true,   length: {maximum: 100 }
+	validates :email, presence: true,   length: {maximum: 100 }
+	validates :phoneNumber, presence: true,   length: {maximum: 100 }
+	validates :semester, presence: true,   length: {maximum: 100 }
+	validates :profName, presence: true,   length: {maximum: 100 }
+	validates :profEmail, presence: true, length: {maximum: 100 } 
+	validates :course_id, presence: true,   length: {maximum: 100 }
+	validates :year, presence: true, length: {maximum: 100 }
+	validates :present_date, presence: true,   length: {maximum: 100 }
+	 
 end
