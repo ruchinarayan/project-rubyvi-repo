adb923b4736fe082763c3dca667ee5c651eb17b7
Ruchi Ruchi <ruchinarayan@gmail.com>
2015-04-08 15:26:39 +0000

    student controller,displayStudent.html.erb --commit

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   post   'login'  => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
 
+  get 'displayStudent' => 'student#displayStudent'
+
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
