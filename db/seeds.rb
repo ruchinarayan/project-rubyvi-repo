44bfe93fb900fdee97013a60a3bc1af7bd0332ab
ssgoli <ssgoli@memphis.edu>
2015-04-09 04:39:37 +0000

    merge done

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,7 +20,6 @@ Course.create(course_id: 'COMP7012', course_name: 'Software Engineering' )
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-Honor.create(contract_id: 00001,  uid: U00564017,  course_id: comp7012,  prof_email: sflem@memphis.edu,  semester: spring,  year: 2015,  grade: A,  pdf: "",  dates: 2015-04-08)
 
 Pending.create(contract_id: 1245, uid: "U0005355", firstName: "Krishna", lastName: "chikkala", email: "hsirrk341@gmail.com", phoneNumber: "9016049107", semester: "fall2014", profName: "Ravi", profEmail: "asdkf@memphis.edu", course_id: "3827", year:2012, present_date: Date.new(1955,3,19))
 
