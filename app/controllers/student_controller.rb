257716dc4d2393311cc5bd150dadcc7539954a90
Ruchi Ruchi <ruchinarayan@gmail.com>
2015-04-13 05:26:49 +0000

    Display Student Commit

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -18,4 +18,24 @@ if @student.update(params.require(:student).permit(:UID, :firstName, :lastName, 
       redirect_to message_student_path
   end
 end
+
+def index
+  		if params[:search] #if value exists
+  		#@pendings= Pending.Keyword_search (params[:search]) # going to Keyword_search method in Pending model class
+  		#@pendings = Pending.find_by uid: 'U0005355'
+      #@pendings = Pending.where(uid: (params[:search]) ).find_each
+
+     @pendings = Pending.where(uid: params[:search])
+     @student = Pending.where(uid: params[:search] ).take
+
+     # @pendings = Pending.where("uid = ?",params[:search])
+      #@student = Pending.where("uid = ?",params[:search]).take
+
+     #@pendings = Pending.where(uid: 'U0005355')
+     #@student = Pending.where(uid: 'U0005355').take
+    else
+    	@pendings= Pending.all
+    end
+
+    end
 end
