38ccc79de547fcf2d57c5e9956eedf3b0e21dcd9
iqbaleric <iqbaleric@yahoo.com>
2015-04-14 01:41:44 +0000

    signUp gone

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150409043621) do
+ActiveRecord::Schema.define(version: 20150413235320) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "course_id"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 20150409043621) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
