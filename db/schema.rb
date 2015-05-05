fdddece22cb1c907e77ff6bdd2c0a092f7d4cef5
fparkins <fparkins@memphis.edu>
2015-05-05 22:14:50 +0000

    schemachange

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20150504211113) do
 
   create_table "checklists", force: :cascade do |t|
