bc38cd6f6fb335b163fca73e09265d6e015e5c88
hsirrk341 <hsirrk341@gmail.com>
2015-05-05 20:32:46 +0000

    title code and links

--- app/models/checklist.rb
+++ app/models/checklist.rb
@@ -1,2 +1,17 @@
+# == Schema Information
+#
+# Table name: checklists
+#
+#  id         :integer          not null, primary key
+#  uid        :string
+#  unhp       :boolean
+#  honexpju   :boolean
+#  honexpse   :boolean
+#  honthese   :boolean
+#  gpa        :boolean
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Checklist < ActiveRecord::Base
 end
--- test/models/checklist_test.rb
+++ test/models/checklist_test.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: checklists
+#
+#  id         :integer          not null, primary key
+#  uid        :string
+#  unhp       :boolean
+#  honexpju   :boolean
+#  honexpse   :boolean
+#  honthese   :boolean
+#  gpa        :boolean
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class ChecklistTest < ActiveSupport::TestCase
